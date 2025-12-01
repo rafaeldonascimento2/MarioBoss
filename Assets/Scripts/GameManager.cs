859355412c8065652df257f8ec5253a08f97c5ae
IsaBertocco <111938842+isabertocco05@users.noreply.github.com>
2025-12-01 13:39:43 -0300

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -73,3 +73,14 @@ public class GameManager : MonoBehaviour
 
     
 }
+
+public class FimDaFase : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.instance.ChecarVitoria();
+        }
+    }
+}
